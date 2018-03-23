58b34bcc8d476b0026954c51bb6e1078b180a7db
wangwei <1564413823@qq.com>
2018-03-23 18:13:44 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import(
 
 func main(){
     http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-	      fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	    fmt.Println("Hello", r.URL.Path)
+	    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
     })
     http.ListenAndServe(":8080", nil)
 }
