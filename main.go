735f8eb3b5e4e2314ef4f969e4a6884e46209adf
wangwei <1564413823@qq.com>
2018-12-24 14:24:27 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import(
-    "fmt"
-    "net/http"
-    "html"
+    	"fmt"
+    	"net/http"
+    	"html"
+	"os"
 )
 
 func main(){
-    http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-	    fmt.Println("Hello", r.URL.Path)
-	    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })
-    http.ListenAndServe(":8080", nil)
+    	f, err := os.OpenFile("/log/test/log", os.O_APPEND|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+	}
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		f.WriteString(r.URL.Path)
+		fmt.Println("Hello", r.URL.Path)
+	    	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    	})
+   	http.ListenAndServe(":8080", nil)
 }
